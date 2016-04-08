e8698edd4bf565fbd0b220711040a0ea3f99e109
Angie Zazueta <angie_zazueta@hotmail.com>
2016-04-08 15:08:30 -0700

    models and controllers done

---
+++ db/migrate/20160408193500_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.text :content
+      t.integer :user_id
+      t.string :image_url
+
+      t.timestamps null: false
+    end
+  end
+end
