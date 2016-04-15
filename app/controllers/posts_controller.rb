94e65a0d35855c6e7a031fed2c6685baf4d72363
Angie Zazueta <angie_zazueta@hotmail.com>
2016-04-14 17:32:58 -0700

    heroku

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
 
   def index
     @user = current_user
-    @posts = Post.paginate(:page => params[:page], :per_page => 3)
+    @posts = Post.order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
   end
 
   def show
